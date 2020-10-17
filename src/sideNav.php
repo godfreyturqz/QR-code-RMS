0056a3efebc57f7cff5c03fef0fa016975acfd27
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-10-17 21:04:19 +0800

    change hover style

--- src/sideNav.php
+++ src/sideNav.php
@@ -1,21 +1,23 @@
 <style>
-    .sidebar{
+    .sidebar-link{
         color: #b6cdff;
         font-size: 18px;
     }
-    .sidebar:hover{
+    .sidebar-link:hover{
         color: white;
+        background-color: #1F4E79;
     }
 </style>
-<nav id="sidebar" class="col-2 p-0 nav flex-column" style="background-color:#293142;height:1100px;display:none;">
+<nav id="sidebar" class="p-0 flex-column" style="background-color:#293142;height:938px;width:300px;display:none;">
     <div class="m-3 mb-4">
-        <img src="images/logo.png" alt="logo">
+        <img src="images/logo.png" alt="logo" style="width:50px;">
+        <span class="text-white ml-3">Name of organization</span>
     </div>
-    <a href="index.php" class="nav-link ml-3 sidebar"><i class="fa fa-home fa-lg mr-3"></i>Home</a>
-    <a href="register.php" class="nav-link ml-3 sidebar"><i class="fa fa-user-circle-o mr-3"></i>Register</a>
-    <a href="login.php" class="nav-link ml-3 sidebar"><i class="fa fa-sign-in mr-3"></i> Log-in</a>
-    <a href="timerecords.php" class="nav-link ml-3 sidebar"><i class="fa fa-clock-o mr-3"></i>Time Records</a>
-    <a href="database.php" class="nav-link ml-3 sidebar"><i class="fa fa-database mr-3"></i>User Records</a>
-    <a href="settings.php" class="nav-link ml-3 sidebar"><i class="fa fa-cog mr-3"></i>Settings</a>
-    <a href="includes/logout.php" class="nav-link ml-3 sidebar"><i class="fa fa-sign-out mr-3"></i>Logout</a>
+    <a href="index.php" class="nav-link ml-3 sidebar-link"><i class="fa fa-home fa-lg mr-3"></i>Home</a>
+    <a href="register.php" class="nav-link ml-3 sidebar-link"><i class="fa fa-user-circle-o mr-3"></i>Register</a>
+    <a href="login.php" class="nav-link ml-3 sidebar-link"><i class="fa fa-sign-in mr-3"></i> Log-in</a>
+    <a href="timerecords.php" class="nav-link ml-3 sidebar-link"><i class="fa fa-clock-o mr-3"></i>Time Records</a>
+    <a href="database.php?page=1&limitRecords=10" class="nav-link ml-3 sidebar-link"><i class="fa fa-database mr-3"></i>User Records</a>
+    <a href="settings.php" class="nav-link ml-3 sidebar-link"><i class="fa fa-cog mr-3"></i>Settings</a>
+    <a href="includes/logout.php" class="nav-link ml-3 sidebar-link"><i class="fa fa-sign-out mr-3"></i>Logout</a>
 </nav>
