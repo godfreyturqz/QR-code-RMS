ee89b5764871d864a14b45ff3984ee45d62e0c05
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-11-15 16:39:25 +0800

    added composer.json

--- src/includes/conn.php
+++ src/includes/conn.php
@@ -1,7 +1,7 @@
 <?php
 $dbServername = "localhost";
 $dbUsername = "root";
-$dbPassword = "123456";
+$dbPassword = "";
 $dbName = "proj2";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
